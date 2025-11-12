0831c371af75427e9d3fdb78ad71e806c89d515a
LordSpoon <valentin.rossi@etu.univ-amu.fr>
2025-11-12 08:25:31 +0100

    first commit

---
+++ app/src/main/java/com/example/applicationandroidrossivalentin/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.applicationandroidrossivalentin
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.applicationandroidrossivalentin.ui.theme.ApplicationAndroidRossiValentinTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ApplicationAndroidRossiValentinTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ApplicationAndroidRossiValentinTheme {
+        Greeting("Android")
+    }
+}
