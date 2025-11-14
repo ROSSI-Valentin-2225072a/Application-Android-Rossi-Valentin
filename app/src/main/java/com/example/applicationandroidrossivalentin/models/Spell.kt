b15a9e4d66d6cf771858dbd94d34e1ef6b3141d3
LordSpoon <valentin.rossi@etu.univ-amu.fr>
2025-11-14 12:34:46 +0100

    oskour

---
+++ app/src/main/java/com/example/applicationandroidrossivalentin/models/Spell.kt
@@ -0,0 +1,94 @@
+package com.example.applicationandroidrossivalentin.models
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Spell(
+    @SerialName("index")
+    val index: String = "",
+
+    @SerialName("name")
+    val name: String = "",
+
+    @SerialName("desc")
+    val desc: List<String> = emptyList(),
+
+    @SerialName("higher_level")
+    val higherLevel: List<String> = emptyList(),
+
+    @SerialName("range")
+    val range: String = "",
+
+    @SerialName("components")
+    val components: List<String> = emptyList(),
+
+    @SerialName("material")
+    val material: String? = null,
+
+    @SerialName("ritual")
+    val ritual: Boolean = false,
+
+    @SerialName("duration")
+    val duration: String = "",
+
+    @SerialName("concentration")
+    val concentration: Boolean = false,
+
+    @SerialName("casting_time")
+    val castingTime: String = "",
+
+    @SerialName("level")
+    val level: Int = 0,
+
+    @SerialName("attack_type")
+    val attackType: String? = null,
+
+    @SerialName("damage")
+    val damage: Damage? = null,
+
+    @SerialName("area_of_effect")
+    val areaOfEffect: AreaOfEffect? = null,
+
+    @SerialName("school")
+    val school: ApiReference? = null,
+
+    @SerialName("classes")
+    val classes: List<ApiReference> = emptyList(),
+
+    @SerialName("subclasses")
+    val subclasses: List<ApiReference> = emptyList()
+)
+
+@Serializable
+data class AreaOfEffect(
+    @SerialName("type")
+    val type: String = "",
+
+    @SerialName("size")
+    val size: Int = 0
+)
+
+@Serializable
+data class Damage(
+    @SerialName("damage_type")
+    val damageType: ApiReference? = null,
+
+    @SerialName("damage_at_slot_level")
+    val damageAtSlotLevel: Map<String, String>? = null,
+
+    @SerialName("damage_at_character_level")
+    val damageAtCharacterLevel: Map<String, String>? = null
+)
+
+@Serializable
+data class ApiReference(
+    @SerialName("index")
+    val index: String = "",
+
+    @SerialName("name")
+    val name: String = "",
+
+    @SerialName("url")
+    val url: String = ""
+)
--- app/src/main/java/com/example/applicationandroidrossivalentin/repositories/SpellRepository.kt
+++ app/src/main/java/com/example/applicationandroidrossivalentin/repositories/SpellRepository.kt
@@ -1,7 +1,38 @@
 package com.example.applicationandroidrossivalentin.repositories
 
+import android.util.Log
+import com.example.applicationandroidrossivalentin.models.Spell
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.engine.cio.CIO
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logger
+import io.ktor.client.plugins.logging.Logging
+import io.ktor.client.plugins.logging.SIMPLE
+import io.ktor.client.request.request
+import io.ktor.serialization.kotlinx.json.json
+
 class SpellRepository {
+    val client = HttpClient(CIO) {
+        install(ContentNegotiation){
+            json()
+        }
 
+        install(Logging) {
+            logger = Logger.SIMPLE
+            level = LogLevel.ALL
+            logger = object : Logger {
+                override fun log(message: String) {
+                    Log.d("Ktor-Logger", message)
+                }
+            }
+        }
+    }
 
+    suspend fun getSpell(index: Int) : Spell {
+        val url = "https://www.dnd5eapi.co/api/2014/spells/$index"
 
+        return client.request(url).body()
+    }
 }
